36c8997ed9718b36c60e9d0d421b2294f433926b
Eric S. Howell <azcoigreach@gmail.com>
2025-08-27 12:44:32 -0700

    Update setup.py version to 0.1.2 to match engine version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="adventure-engine",
-    version="0.1.0",
+    version="0.1.2",
     packages=find_packages(),
     py_modules=["adventure"],
     install_requires=["click"],
